234bf1d55eb01298de9bb4d7485ab78ae227e549
Kevin Marmolejo <kevin.marmolejo@uao.edu.co>
2022-01-10 23:30:15 -0500

    Add all component to landing page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import Head from "next/head";
 import Nav from "components/CustomNav";
-import Tag from "components/Tag";
 import styles from "styles/Home.module.css";
-import { Row, Col, Image } from "react-bootstrap";
 import Skills from "components/Skills";
+import React from "react";
+import { firestore } from "controller/client";
+import Projects from "components/Projects";
+import AboutMe from "components/AboutMe";
+import Contact from "components/Contact";
+import CustomFooter from "components/CutomFooter";
 
 export default function Home() {
+  React.useEffect(() => {
+    firestore
+      .collection("test")
+      .doc("test")
+      .get()
+      .then((res) => {
+        console.log(res.data());
+      });
+  }, []);
   return (
     <>
       <Head>
@@ -18,37 +31,18 @@ export default function Home() {
         <Nav />
 
         {/* About me */}
-        <section className={styles.about_me} id="about_me">
-          <Row lg={2} md={2} xs={1}>
-            <Col>
-              <div className={styles.tags}>
-                <Tag text="Multimedia Engineer" />
-                <Tag text="Full Stack Developer" />
-              </div>
-              <h1>Title</h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </Col>
-            <Col>
-              <Image
-                className={styles.image}
-                fluid
-                src="/png/photo.png"
-                alt="Kevin Marmolejo"
-              />
-            </Col>
-          </Row>
-        </section>
+        <AboutMe />
 
         {/* Skills */}
         <Skills />
+
+        {/* Projects */}
+        <Projects />
+
+        {/* Contact */}
+        <Contact />
+
+        <CustomFooter />
       </main>
     </>
   );
